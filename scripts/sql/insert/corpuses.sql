7b0291a07acce2cfd174758e8dc6e5aea8fd2048
HumanG33k <HumanG33k@users.noreply.github.com>
2016-10-17 01:38:51 +0200

    Add default values from visablis website.

---
+++ scripts/sql/insert/corpuses.sql
@@ -0,0 +1,2 @@
+INSERT INTO Corpuses (id, name, visiblis_number, description, created) VALUES
+('64d3db94-85d5-404e-b7c0-f07d3ce2adc6' ,'Visiblis', '0', 'Corpus par défaut de Visiblis. Également celui utilisé par défaut sur Illumination', now() );
