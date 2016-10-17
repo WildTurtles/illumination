da1276c82dfc77a72583e0b88387f74bf2b9de29
HumanG33k <HumanG33k@users.noreply.github.com>
2016-10-17 03:19:56 +0200

    Modify the readme, add some explanations. Add a script to create databases tables.

---
+++ scripts/create_db.sh
@@ -0,0 +1,41 @@
+#!/bin/bash 
+
+dbuser=illumination
+dbname=illumination
+
+#users and groups
+psql -U $dbuser -d $dbname -f ./sql/create/groups_users.sql
+
+# app configuration
+psql -U $dbuser -d $dbname -f ./sql/create/configurations.sql
+
+# formats for responses
+psql -U $dbuser -d $dbname -f ./sql/create/formats.sql
+psql -U $dbuser -d $dbname -f ./sql/insert/formats.sql
+
+# formats for responses
+psql -U $dbuser -d $dbname -f ./sql/create/corpus.sql
+psql -U $dbuser -d $dbname -f ./sql/insert/corpus.sql
+
+# accounts for request
+psql -U $dbuser -d $dbname -f ./sql/create/accounts.sql
+
+#visiblis supported languages 
+psql -U $dbuser -d $dbname -f ./sql/create/languages.sql
+psql -U $dbuser -d $dbname -f ./sql/insert/languages.sql
+
+#categories for semantic request (url title text)
+psql -U $dbuser -d $dbname -f ./sql/create/categories.sql
+psql -U $dbuser -d $dbname -f ./sql/insert/categories.sql
+
+#keyword send by the api 
+psql -U $dbuser -d $dbname -f ./sql/create/keywords.sql
+
+#the semantic request
+psql -U $dbuser -d $dbname -f ./sql/create/semantic_requests.sql
+
+#the results for keyword to a request
+psql -U $dbuser -d $dbname -f ./sql/create/keyword_link_requests.sql
+
+#some metrics send by the api
+psql -U $dbuser -d $dbname -f ./sql/create/semantic_responses.sql
